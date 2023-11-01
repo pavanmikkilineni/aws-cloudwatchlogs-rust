b29893ae1d736aee8bf99afd28569d45df6537cc
Kumar Mikkilineni Pavan (MS/ECL2) <wmu2kor@KOR-V-002FX.kor.apac.bosch.com>
2023-11-01 13:48:21 +0530

    added aws-cloudwatchlogs-rust example

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use aws_sdk_cloudwatchlogs as cloudwatchlogs;
+use cloudwatchlogs::types::InputLogEvent;
+
+#[::tokio::main]
+async fn main() -> Result<(), cloudwatchlogs::Error> {
+    // Load AWS configuration from environment variables
+    let config = aws_config::load_from_env().await;
+    let client = aws_sdk_cloudwatchlogs::Client::new(&config);
+
+    // Define constants for the log group and log stream names
+    const DISTI_LOG_GROUP_NAME: &str = "Disti Logs";
+    const DISTI_LOG_STREAM_NAME: &str = "Application Logs";
+
+    // Create or retrieve the log group, and add tags
+    let disti_log_group = client
+        .create_log_group()
+        .log_group_name(DISTI_LOG_GROUP_NAME)
+        .tags("Environment", "Test");
+    
+    // Send the request to create the log group
+    match disti_log_group.send().await {
+        Ok(response) => {
+            println!("Log group created or already exists: {:?}", response);
+        }
+        Err(err) => {
+            println!("Error creating log group: {:?}", err);
+        }
+    };
+
+    // Create or retrieve the log stream
+    let disti_log_stream = client
+        .create_log_stream()
+        .log_group_name(DISTI_LOG_GROUP_NAME)
+        .log_stream_name(DISTI_LOG_STREAM_NAME);
+
+    // Send the request to create the log stream
+    match disti_log_stream.send().await {
+        Ok(response) => {
+            println!("Log stream created or already exists: {:?}", response);
+        }
+        Err(err) => {
+            println!("Error creating log stream: {:?}", err);
+        }
+    }
+
+    // Create a log event
+    let disti_log_event = InputLogEvent::builder()
+        .timestamp(1)
+        .message("Hello, World!")
+        .build();
+
+    // Put the log event into the log stream
+    let disti_put_log_event = client.put_log_events()
+        .log_group_name(DISTI_LOG_GROUP_NAME)
+        .log_stream_name(DISTI_LOG_STREAM_NAME)
+        .log_events(disti_log_event);
+
+    // Send the request to put the log event
+    match disti_put_log_event.send().await {
+        Ok(response) => {
+            println!("Log event sent successfully: {:?}", response);
+        }
+        Err(err) => {
+            println!("Error sending log event: {:?}", err);
+        }
+    }
+
+    Ok(())
+}
